256aebfa76d55f299ce3331b5eb2d5c2918415eb
Frozen <mrozix11@gmail.com>
2024-02-23 22:17:33 +0100

    Add s-expression support

--- build.rs
+++ build.rs
@@ -307,6 +307,11 @@ fn main() {
             extra_files: vec!["scanner.c"],
         },
         TreeSitterParser {
+            name: "tree-sitter-sexp",
+            src_dir: "vendored_parsers/tree-sitter-sexp-src",
+            extra_files: vec![],
+        },
+        TreeSitterParser {
             name: "tree-sitter-scss",
             src_dir: "vendored_parsers/tree-sitter-scss-src",
             extra_files: vec!["scanner.c"],
